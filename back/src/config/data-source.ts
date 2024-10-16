9a649eaa081f9124152f0c9a2c9c0e035ade9bde
JuanMatiasGema12 <166863870+JuanMatiasGema12@users.noreply.github.com>
2024-10-16 20:28:15 -0300

    Update data-source.ts

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -1,25 +1,26 @@
-import { DataSource } from "typeorm"
+
 import { User } from "../entities/UserEntity"
-import { Appointment } from "../entities/AppointmentEntity"
 import { Credential } from "../entities/CredentialEntity"
 
+
+import { DataSource } from "typeorm";
+import { DB_HOST, DB_LOGIN, DB_NAME, DB_SYNC, DB_USERNAME, PASSWORD_DB } from "./envs";
+
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
+    type:  "postgres",
+    host: DB_HOST,
     port: 5432,
-    username: "postgres",
-    password: "matiasgema123456",
-    database: "demo_typeorm",
-    //dropSchema: true,
-    synchronize: true,
-    logging: false,
-    entities: [User, Appointment, Credential],
+    username: DB_USERNAME,
+    password: PASSWORD_DB,
+    database: DB_NAME,
+    synchronize: DB_SYNC,
+    logging: DB_LOGIN,
+    dropSchema: true,
+    entities: ["src/entities/*/.ts"],
     subscribers: [],
     migrations: [],
 })
 
 export const UserModel = AppDataSource.getRepository(User)
-
-export const AppointmentModel = AppDataSource.getRepository(Appointment)
 
 export const CredentialModel = AppDataSource.getRepository(Credential)
